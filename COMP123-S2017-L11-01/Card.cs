0c1540af3e14c1ac83ea6f1c10b8ae31d080bb75
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-26 22:52:24 -0400

    Implemented the IClonable interface and Added the OverWrite method

--- COMP123-S2017-L11-01/Card.cs
+++ COMP123-S2017-L11-01/Card.cs
@@ -6,17 +6,18 @@ using System.Text;
 /*
  * Name: Yanying Zhang
  * Date: July 25, 2017
- * Description: This is the face enum
- * Version: 0.1 -- added Card class
+ * Description: This is the Card class
+ * Version: 0.2 -- Implemented the IClonable interface
+ *                  Added the OverWrite method
  */
 namespace COMP123_S2017_L11_01
 {
-    public class Card
+    public class Card : ICloneable
     {
         // PRIVATE INSTANCE VARIABLES
         private Face _face;
         private Suit _suit;
-
+        
         // PUBLIC PROPERTITES
         public Face Face {
             get
@@ -28,19 +29,48 @@ namespace COMP123_S2017_L11_01
                 this._face = value;
             }
         }
+
+        public Suit Suit
+        {
+            get
+            {
+                return this._suit;
+            }
+            set
+            {
+                this._suit = value;
+            }
+        }
+
         // CONSTRUCTORS
+
         /// <summary>
         /// This is the main constructors
-        /// It take two arguments - face(face), suit(Suit)
+        /// It takes two arguments - face(face), suit(Suit)
         /// </summary>
         /// <param name="face"></param>
         /// <param name="suit"></param>
         public Card(Face face, Suit suit)
         {
-            this._face = Face;
-            this._suit = suit;
+            this.Face = face;
+            this.Suit = suit;
         }
         // PRIVATE METHODS
         // PUBLIC METHODS
+
+            /// <summary>
+            /// This method overrides the built-in Clone method
+            /// </summary>
+            /// <returns></returns>
+        public object Clone()
+        {
+            return new Card(this.Face, this.Suit);
+        }
+
+        public static void OverWrite(Card first, Card second)
+        {
+            first.Face = second.Face;
+            first.Suit = second.Suit;
+        }
     }
 }
---
+++ COMP123-S2017-L11-01/Deck.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Yanying Zhang
+ * Date: July 25, 2017
+ * Description: This is the Deck class
+ * It inherits from the List generic and uses Card as the base
+ * Version: 0.2 -- implemented the Iclonoeable interface
+ */
+namespace COMP123_S2017_L11_01
+{
+    public class Deck : List<Card>
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private Random _random;
+        private Face _face;
+
+        // PRIVATE PROPERTITES
+        private Random Random
+        {
+            get
+            {
+                return this._random;
+            }
+        }
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// This is the initialize methods
+        /// </summary>
+        public Deck()
+        {
+            this._initialize();
+        }
+        // PRIVATE METHODS
+
+        private void _initialize()
+        {
+            //initialize the presude random nunber generator
+            this._random = new Random();
+
+            //this builts the Desk, fulls it with cards;
+
+            for(int suit = (int)(Suit.Clubs); suit<= (int)Suit.Spades; suit++)
+            {
+                for(int face = (int)(Face.Ace); face<= (int)Face.King; face++)
+                {
+                    this.Add(new Card((Face)face, (Suit)suit));
+                }
+            }
+        }
+
+        // PUBLIC METHODS
+
+
+
+        public override string ToString()
+        {
+            string outputString = "";
+            foreach(Card card in this)
+            {
+                outputString += "The " + card.Face + " of " + card.Suit + "\n";
+            }
+            return outputString;
+        }
+
+        /// <summary>
+        /// This method shuffles the deck by using random indices 
+        /// </summary>
+        public void Shuffle()
+        {
+            int firstCard;
+            int secondCard;
+            Card tempCard;
+
+            for (int card = 0; card < this.Count; card++)
+            {
+                firstCard = this.Random.Next(0,52);
+                secondCard = this.Random.Next(0, 52);
+
+                tempCard = (Card)this[secondCard].Clone();
+                Card.Overwrite(this[secondCard], this[firstCard]);
+                Face.Overwrite(this[secondCard], this[firstCard]);
+
+                this[secondCard].Face = this[firstCard].Face;
+                this[secondCard].Suit = this[firstCard].Suit;
+                this[firstCard].Face = tempCard.Face;
+                this[firstCard].Suit = tempCard.Suit;
+            }
+        }
+
+
+
+
+    }
+}
