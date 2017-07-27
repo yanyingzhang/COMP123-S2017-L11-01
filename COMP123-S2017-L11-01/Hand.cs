8d8d99d543490ffd354b3dd88d94a47c15850e9c
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-07-27 13:15:36 -0400

    created the Hand Class

---
+++ COMP123-S2017-L11-01/Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Yanying Zhang
+ * Date: July 27, 2017
+ * Description: This is the Hand class which inherited from CardList
+ * Version: 0.1 -- created the Hand Class
+ */
+namespace COMP123_S2017_L11_01
+{
+    public class Hand : CardList
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTITES
+        
+        // CONSTRUCTORS
+
+        // PRIVATTE METHODS
+        protected override void _initialize()
+        {
+            // this method is empty
+        }
+
+        // PUBLIC METHODS
+    }
+}
